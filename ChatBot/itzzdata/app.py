4b2730a15419d1e2aaf7c96b84252bdd19db2acb
itsmeSugunakar <sugun.sr@gmail.com>
2025-05-25 02:03:05 -0400

    Created itzzdata portal static page!

--- ChatBot/itzzdata/app.py
+++ ChatBot/itzzdata/app.py
@@ -27,4 +27,4 @@ if __name__ == '__main__':
     # When running locally, the app will be accessible at http://127.0.0.1:5000/
     # For deployment, this will be handled by your deployment environment (e.g., Gunicorn, uWSGI)
     # and integrated with API Gateway.
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=9000)
